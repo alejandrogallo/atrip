c45c0c4420d769ed61cc55fe5b5ebb5c3b45d6e2
Alejandro Gallo <aamsgallo@gmail.com>
2021-09-01 21:30:02 +0200

    Update benches

--- bench/test_main.cxx
+++ bench/test_main.cxx
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <atrip.hpp>
+#include <ctf.hpp>
+#include <bench/hauta.h>
 
-int main() {
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+  CTF::World world(argc, argv);
+
+  const int no(hauta::option<int>(argc, argv, "--no"))
+          , nv(hauta::option<int>(argc, argv, "--nv"))
+          ;
+
+  std::vector<int> symmetries(4, NS)
+                 , vo({nv, no})
+                 , vvoo({nv, nv, no, no})
+                 , ooov({no, no, no, nv})
+                 , vvvo({nv, nv, nv, no})
+                 ;
+
+  CTF::Tensor<double>
+      ei(1, ooov.data(), symmetries.data(), world)
+    , ea(1, vo.data(), symmetries.data(), world)
+    , Tph(2, vo.data(), symmetries.data(), world)
+    , Tpphh(4, vvoo.data(), symmetries.data(), world)
+    , Vpphh(4, vvoo.data(), symmetries.data(), world)
+    , Vhhhp(4, ooov.data(), symmetries.data(), world)
+    , Vppph(4, vvvo.data(), symmetries.data(), world)
+    ;
+
   std::cout << "Hello world" << std::endl;
+  MPI_Finalize();
   return 0;
 }
