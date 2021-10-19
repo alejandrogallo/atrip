9c56870dcf57c4bdfc98903b24c5d560291b0c74
Alejandro Gallo <aamsgallo@gmail.com>
2021-10-19 16:18:44 +0200

    Update ctf make infrastructure

--- atrip.mk
+++ atrip.mk
@@ -1,2 +1,8 @@
 ATRIP_ROOT := $(abspath $(PWD))
 CXXFLAGS += -I$(ATRIP_ROOT)/include
+
+CTF_COMMIT         = 968f8f9eb6aab1d6b67d2fcc1a70c9fc3b98adfa
+CTF_GIT_REPOSITORY = https://github.com/cc4s/ctf
+CTF_BUILD_PATH     = $(ATRIP_ROOT)/extern/build/$(CONFIG)/ctf/$(CTF_COMMIT)
+CTF_SRC_PATH       = $(ATRIP_ROOT)/extern/src/$(CONFIG)/ctf/$(CTF_COMMIT)
+CXXFLAGS          += -I$(CTF_BUILD_PATH)/include
--- etc/make/ctf.mk
+++ etc/make/ctf.mk
@@ -1,4 +1,28 @@
-include ./etc/make/ctf_vars.mk
-include ./etc/make/ctf_rules.mk
+CTF_CONFIG_FLAGS =
+CTF_STATIC_LIB = $(CTF_BUILD_PATH)/lib/libctf.a
+CTF_SHARED_LIB = $(CTF_BUILD_PATH)/lib/libctf.so
+CTF_GIT_REPOSITORY ?= https://github.com/cyclops-community/ctf
+
+$(CTF_SRC_PATH)/configure:
+	mkdir -p $(@D)
+	git clone $(CTF_GIT_REPOSITORY) $(@D)
+	cd $(@D) && git checkout $(CTF_COMMIT)
+
+$(CTF_BUILD_PATH)/Makefile: $(CTF_SRC_PATH)/configure
+	mkdir -p $(CTF_BUILD_PATH)
+	cd $(CTF_BUILD_PATH) && $(CTF_SRC_PATH)/configure $(CTF_CONFIG_FLAGS)
+
+$(CTF_STATIC_LIB): $(CTF_BUILD_PATH)/Makefile
+	$(info Compiling $@)
+	cd $(CTF_BUILD_PATH) && $(MAKE)
+
+.PHONY: ctf ctf-clean
+ctf: $(CTF_STATIC_LIB)
+
+ctf-clean:
+	rm -rf $(CTF_BUILD_PATH)
 
 EXTERNAL_DEPENDENCIES += ctf
+STATIC_LIBS += $(CTF_LDFLAGS)
+INCLUDE_FLAGS += $(CTF_INCLUDE)
+REQUIRED_MAKEVARS += CTF_COMMIT CTF_BUILD_PATH CTF_GIT_REPOSITORY CTF_SRC_PATH
