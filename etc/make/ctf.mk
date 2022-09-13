5c41fb65e491a1cd860d151b8f04399440b77f2c
Gallo Alejandro <gallo@l55.vsc.xcat>
2022-09-13 14:15:39 +0200

    Fix ctf.mk for CUDA

--- etc/make/ctf.mk
+++ etc/make/ctf.mk
@@ -14,10 +14,17 @@ $(CTF_SRC_PATH)/configure:
 	git clone $(CTF_GIT_REPOSITORY) $(@D)
 	cd $(@D) && git checkout $(CTF_COMMIT)
 
+# Here make sure that ctf does not builld with CUDA support
+# since it is broken anyways
+# 
+# Also we patch the file kernel.h because it mostl
+# doesn't work when we try to include ctf in a CUDACC
+# compiler code.
 $(CTF_BUILD_PATH)/Makefile: $(CTF_SRC_PATH)/configure
 	mkdir -p $(CTF_BUILD_PATH)
 	cd $(CTF_BUILD_PATH) && \
-		$(abspath $(CTF_SRC_PATH))/configure $(CTF_CONFIG_FLAGS)
+		$(abspath $(CTF_SRC_PATH))/configure NVCC="" $(CTF_CONFIG_FLAGS)
+	sed -i s/CUDACC/ATRIP_NOT_CUDACC/g ${CTF_SRC_PATH}/src/interface/kernel.h
 
 
 $(CTF_STATIC_LIB): $(CTF_BUILD_PATH)/Makefile
