273ca59d7e337dedd151d0d7fccc993542d5f721
Alejandro Gallo <aamsgallo@gmail.com>
2023-04-21 14:19:39 +0200

    Add Malloc wrappers for memory initialization

---
+++ include/atrip/Malloc.hpp
@@ -0,0 +1,24 @@
+#include <atrip/CUDA.hpp>
+#include <type_traits>
+
+#if defined(ATRIP_DRY)
+
+#  define MALLOC_HOST_DATA(msg, ptr, size) *(void **)ptr = (void *)malloc(16)
+
+#  if defined(HAVE_CUDA)
+#    define MALLOC_DATA_PTR(msg, ptr, _size) _CUDA_MALLOC(msg, ptr, 16)
+#  else
+#    define MALLOC_DATA_PTR(msg, ptr, _size) MALLOC_HOST_DATA(msg, ptr, 16)
+#  endif /* defined(HAVE_CUDA) */
+
+#else
+
+#  define MALLOC_HOST_DATA(msg, ptr, size) *(void **)ptr = (void *)malloc(size)
+
+#  if defined(HAVE_CUDA)
+#    define MALLOC_DATA_PTR(msg, ptr, _size) _CUDA_MALLOC(msg, ptr, _size)
+#  else
+#    define MALLOC_DATA_PTR(msg, ptr, _size) MALLOC_HOST_DATA(msg, ptr, _size)
+#  endif /* defined(HAVE_CUDA) */
+
+#endif /* defined(ATRIP_DRY) */
