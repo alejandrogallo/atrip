5f431595c9a3af3f1244864702cc91a023a29dd0
Alejandro Gallo <aamsgallo@gmail.com>
2023-05-09 18:15:49 +0200

    Update ctf version

--- atrip.mk
+++ atrip.mk
@@ -1,6 +1,6 @@
 
 if WITH_BUILD_CTF
-CTF_COMMIT         = 968f8f9eb6aab1d6b67d2fcc1a70c9fc3b98adfa
+CTF_COMMIT         = 53ae5daad851bf3b198ebe1fa761c13b12291116
 CTF_GIT_REPOSITORY = https://github.com/cc4s/ctf
 CTF_BUILD_PATH     = $(top_builddir)/extern/build/ctf/$(CTF_COMMIT)
 CTF_SRC_PATH       = $(top_builddir)/extern/src/ctf/$(CTF_COMMIT)
