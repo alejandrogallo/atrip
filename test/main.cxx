1f944c568cdafef95631ee2b59807a0c3340813c
Alejandro Gallo <aamsgallo@gmail.com>
2023-05-09 18:15:33 +0200

    Add some tests for operations

--- test/main.cxx
+++ test/main.cxx
@@ -17,59 +17,77 @@
 #include <cassert>
 // [[[[file:~/software/atrip/atrip.org::*Tests][Tests]]][]]
 #include <atrip/Checkpoint.hpp>
+#include <atrip/Operations.hpp>
 using namespace atrip;
 // ends here
 
-
-
-#define TESTCASE(_name, ...) {                  \
-    std::cout << "\x1b[35m-> \x1b[0m"           \
-              <<  _name                         \
-              << std::endl;                     \
-    __VA_ARGS__                                 \
-    }
+#define TESTCASE(_name, ...)                                                   \
+  {                                                                            \
+    std::cout << "\x1b[35m-> \x1b[0m" << _name << std::endl;                   \
+    __VA_ARGS__                                                                \
+  }
 
 int main() {
 
-  // [[[[file:~/software/atrip/atrip.org::*Tests][Tests]]][]]
-  #define _CMP_CHECK(what)                              \
-    std::cout << "\t Checking " << #what  << std::endl; \
-    assert(in.what == what);                            \
-    assert(out.what == what);
-  
-    TESTCASE("Testing checkpoint reader and writers",
-             const std::string out_checkpoint = "/tmp/checkpoint.yaml";
-             const double energy = -1.493926352289995443;
-             const size_t no = 154, nv = 1500, nranks = 48*10, nnodes = 10;
-             const size_t iteration = 546;
-             std::cout << "\twriting to " << out_checkpoint << std::endl;
-  
-             for (bool rankRoundRobin: {true, false}) {
-               atrip::Checkpoint out = {no,
-                                        nv,
-                                        nranks,
-                                        nnodes,
-                                        energy,
-                                        iteration,
-                                        rankRoundRobin}, in;
-  
-  
-               write_checkpoint(out, out_checkpoint);
-               in = read_checkpoint(out_checkpoint);
-  
-               _CMP_CHECK(no);
-               _CMP_CHECK(nv);
-               _CMP_CHECK(nranks);
-               _CMP_CHECK(nnodes);
-               _CMP_CHECK(iteration);
-               _CMP_CHECK(rankRoundRobin);
-               _CMP_CHECK(energy);
-             }
-  
-  
-             )
-  #undef _CMP_CHECK
-  
+// [[[[file:~/software/atrip/atrip.org::*Tests][Tests]]][]]
+#define _CMP_CHECK(what)                                                       \
+  do {                                                                         \
+    std::cout << "\t Checking " << #what << std::endl;                         \
+    assert(in.what == what);                                                   \
+    assert(out.what == what);                                                  \
+  } while (0)
+
+  TESTCASE(
+      "Testing checkpoint reader and writers",
+      const std::string out_checkpoint = "/tmp/checkpoint.yaml";
+      const double energy = -1.493926352289995443;
+      const size_t no = 154, nv = 1500, nranks = 48 * 10, nnodes = 10;
+      const size_t iteration = 546;
+      std::cout << "\twriting to " << out_checkpoint << std::endl;
+
+      for (bool rankRoundRobin
+           : {true, false}) {
+        atrip::Checkpoint
+            out = {no, nv, nranks, nnodes, energy, iteration, rankRoundRobin},
+            in;
+
+        write_checkpoint(out, out_checkpoint);
+        in = read_checkpoint(out_checkpoint);
+
+        _CMP_CHECK(no);
+        _CMP_CHECK(nv);
+        _CMP_CHECK(nranks);
+        _CMP_CHECK(nnodes);
+        _CMP_CHECK(iteration);
+        _CMP_CHECK(rankRoundRobin);
+        _CMP_CHECK(energy);
+      }
+
+  )
+#undef _CMP_CHECK
+
+#define _CHECK(a, b)                                                           \
+  do {                                                                         \
+    std::cout << "\t that " << #a << " is equal to " << #b << "  (" << b       \
+              << ")" << std::endl;                                             \
+    assert(a == b);                                                            \
+  } while (0)
+
+  TESTCASE(
+      "simple operations should work", const double a = 41.549, b = 1321.181;
+      double aa = a, bb = b;
+      _CHECK(atrip::acc::maybeConjugateScalar<double>(a), a);
+      _CHECK(atrip::acc::prod<double>(a, b), a * b);
+      // _CHECK(atrip::acc::real<double>(a), a);
+      _CHECK(atrip::acc::sub<double>(a, b), a - b);
+      _CHECK(atrip::acc::add<double>(a, b), a + b);
+      atrip::acc::sum_in_place(&aa, &bb);
+      _CHECK(aa, a + b);
+
+      /*end*/)
+
+#undef _CHECK
+
   // ends here
 
   return 0;
